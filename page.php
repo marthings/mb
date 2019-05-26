b62d27d11986db41cb9b02e3359720aae50906f0
Martin Berggren <martin@digilar.se>
2019-05-26 15:42:15 +0200

    Basic templates

---
+++ page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since    Timber 0.1
+ */
+$context = Timber::context();
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+Timber::render( array( 'page-' . $timber_post->post_name . '.twig', 'page.twig' ), $context );
